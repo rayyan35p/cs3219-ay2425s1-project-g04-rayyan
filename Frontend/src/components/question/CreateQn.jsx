b375ce06c52bfe4a084ac9f5539ce14aa6ed78ab
elyseleong <elyse.xuan@gmail.com>
2024-09-27 21:13:31 +0800

    Update UI
    - Move Title field to the top
    - Reduce the font size of Complexity header
    - Add margin to the bottom of Complexity container

--- Frontend/src/components/question/CreateQn.jsx
+++ Frontend/src/components/question/CreateQn.jsx
@@ -39,12 +39,17 @@ function CreateQn({handleClose, addQuestion}) {
             <form onSubmit={Submit}>
                 {error && <div className="alert alert-danger">{error}</div>}
                 <div className="mb-2">
+                    <label htmlFor="">Title</label>
+                    <input type="text" placeholder='Shortest Distance' className='form-control'
+                    onChange={(e) => setTitle(e.target.value)}/>
+                </div>
+                <div className="mb-2">
                     <label htmlFor="">Category</label>
                     <input type="text" placeholder='Data Structures' className='form-control'
                     onChange={(e) => setCategory(e.target.value.split(","))}/>
                 </div>
-                <div className="container mt-3">
-                    <h3>Complexity</h3>
+                <div className="container my-3">
+                    <h5>Complexity</h5>
                     <div className="form-check">
                         <input type="radio" id="easy" value="Easy" name={"complexity"} 
                         onChange={(e) => setComplexity(e.target.value)}/>
@@ -66,11 +71,7 @@ function CreateQn({handleClose, addQuestion}) {
                     <input type="text" placeholder='Return the largest....' className='form-control'
                     onChange={(e) => setDescription(e.target.value)}/>
                 </div>
-                <div className="mb-2">
-                    <label htmlFor="">Title</label>
-                    <input type="text" placeholder='Shortest Distance' className='form-control'
-                    onChange={(e) => setTitle(e.target.value)}/>
-                </div>
+
                 <button className="btn btn-success">Submit</button>
             </form>
         </div>
