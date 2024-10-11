4f133b31151df90f84dd8823b7ac846151a76a58
Guo Yuhao <guoyuhao4@gmail.com>
2024-10-12 00:11:04 +0800

    Fix refactor misses

--- Frontend/src/services/matchmaking.js
+++ Frontend/src/services/matchmaking.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3003';
 
 const enterMatchmaking = async (user) => {
-    return await axios.post(`${baseUrl}/api/match/enterQueue`, user);
+    return await axios.post(`${baseUrl}/api/match/enterMatchmaking`, user);
 }
