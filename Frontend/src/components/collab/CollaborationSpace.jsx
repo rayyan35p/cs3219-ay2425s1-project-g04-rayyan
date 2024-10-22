3568eb47260b4999beb9257860743a1d661fadd0
elyseleong <elyse.xuan@gmail.com>
2024-10-22 13:24:08 +0800

    Add Exit button for Collab Space

--- Frontend/src/components/collab/CollaborationSpace.jsx
+++ Frontend/src/components/collab/CollaborationSpace.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const CollaborationSpace = () => {
+    const navigate = useNavigate();
     const { roomId } = useParams(); // Get the roomId from the URL
+
+    const handleExit = () => {
+        navigate("/home")
+    }
 
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Welcome to Collaboration Space</h1>
             <p>You are in room: {roomId}</p>
+            <Button variant="danger" onClick={() => handleExit()}>Exit</Button>{' '}
         </div>
     );
 };
