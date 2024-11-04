f5a00505c421456d30b0f4bf03ade3b3012a1a4e
qiaoen17 <92434807+qiaoen17@users.noreply.github.com>
2024-11-04 23:26:29 +0800

    frontend question auth
    Have not implemented backend authentication for question yet

--- Backend/user-service/routes/auth-routes.js
+++ Backend/user-service/routes/auth-routes.js
@@ -1,12 +1,15 @@
 import express from "express";
 
-import { handleLogin, handleVerifyToken } from "../controller/auth-controller.js";
-import { verifyAccessToken } from "../middleware/basic-access-control.js";
+import { handleLogin, handleVerifyAdmin, handleVerifyToken } from "../controller/auth-controller.js";
+import { verifyAccessToken, verifyIsAdmin } from "../middleware/basic-access-control.js";
 
 const router = express.Router();
 
 router.post("/login", handleLogin);
 
 router.get("/verify-token", verifyAccessToken, handleVerifyToken);
+
+// add new route to check for admin
+router.get("/verify-admin", verifyAccessToken, verifyIsAdmin, handleVerifyAdmin);
 
 export default router;
