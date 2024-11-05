e33c209c9c0e8eea7b2255fb38728da458ae431a
qiaoen17 <92434807+qiaoen17@users.noreply.github.com>
2024-11-05 19:39:25 +0800

    Make code output scrollable

--- Frontend/src/components/collab/CodeSpace.jsx
+++ Frontend/src/components/collab/CodeSpace.jsx
@@ -14,15 +14,17 @@ const CodeSpace = ({ handleEditorChange, loading, code, language, output }) => {
             onChange={handleEditorChange}
             theme='vs-dark'
         />
-        <Container style={{ height: '200px', border: '1px solid #ccc', borderRadius: '0.5rem', padding: '1rem', backgroundColor: '#f8f9fa'}}>
-            <h5>Output</h5>
+        <Container style={{ height: '225px', border: '1px solid #ccc', borderRadius: '0.5rem', padding: '1rem', backgroundColor: '#f8f9fa', padding: '20px'}}>
+            <h4>Output</h4>
             {loading ? (
               <div className="d-flex justify-content-center align-items-center" style={{ height: '100%' }}>
                 <Spinner animation="border" variant="primary" />
                 <span className="ms-1">Running...</span>
               </div>
             ): (
-              <p style={{ fontFamily: 'monospace', whiteSpace: 'pre-wrap'}}>{output}</p>
+              <div style={{ maxHeight: '150px', overflowY: 'auto', fontFamily: 'monospace', whiteSpace: 'pre-wrap', border: '1px solid rgba(0, 0, 0, 0.2)', borderRadius: '0.5rem'}}>
+                <p>{output}</p>
+              </div>
             )}
         </Container>
     </Stack>
