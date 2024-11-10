76d0b55f6bc97fa2287d296d2050496720fa6c15
elyseleong <elyse.xuan@gmail.com>
2024-11-10 20:19:19 +0800

    Remove unused files reference

--- Backend/MatchingService/app.js
+++ Backend/MatchingService/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require("cors");
 const dotenv = require("dotenv");
-const matchmakingRouter = require("./controllers/matchmaking");
 const { consumeQueue, consumeDLQ } = require('./rabbitmq/subscriber');
 const { setupRabbitMQ } = require('./rabbitmq/setup');
 
@@ -12,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.options("*", cors());
-
-app.use('/api/match', matchmakingRouter);
 
 // TODO: Start consuming RabbitMQ queues 
 
